1d54ff64021c72b0802d9b526e873ccb9c088202
Rabby <rabbymbamu@gmail.com>
2022-05-20 16:04:11 +0200

    home fin

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,267 @@
+@extends('layout.partial')
+@section('content')
+    <div class="owl-carousel-wrapper">
+      <div class="box-92819">
+        <div class="owl-carousel slide-one-item-alt-text">
+
+          <div>
+            <h1 class="text-uppercase mb-3">We Are Specialist In The Field of Architect</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus harum, error minima?</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded-0">Contact Us</a></p>
+          </div>
+
+          <div>
+            <h1 class="text-uppercase mb-3">Design That Makes You Feel At Home</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deleniti similique, nesciunt.</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded-0">Contact Us</a></p>
+          </div>
+
+          <div>
+            <h1 class="text-uppercase mb-3">Ready To Start Your Home</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, autem odit quae!</p>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded-0">Contact Us</a></p>
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <div class="owl-carousel owl-1 ">
+        <div class="ftco-cover-1" style="background-image: url('images/hero_1.jpg');"></div>
+        <div class="ftco-cover-1" style="background-image: url('images/hero_2.jpg');"></div>
+        <div class="ftco-cover-1" style="background-image: url('images/hero_3.jpg');"></div>
+        
+      </div>
+    </div>
+
+    
+    <div class="site-section">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-4">
+            <div class="h-100 bg-white box-29291">
+              <h2 class="heading-39291">Welcome To <br> Our Company</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo mollitia id ea ab in! Nam eligendi distinctio, vitae.</p>
+              <p>Alias odit ipsam quas unde obcaecati molestiae consequatur numquam cupiditate perferendis facere, nulla nemo id, accusantium corrupti tempora.</p>
+
+              <p class="mt-5">
+                <span class="d-block font-weight-bold text-black">Bruce Smith</span>
+                <span class="d-block font-weight-bold text-muted">Founder, CEO</span>
+                
+                <img src="images/signature.svg" alt="Image" class="img-fluid" width="140">
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="owl-carousel owl-3">
+              <img src="images/about_1.jpg" alt="Image" class="img-fluid">
+              <img src="images/about_2.jpg" alt="Image" class="img-fluid">
+              <img src="images/about_3.jpg" alt="Image" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 align-items-center">
+          <div class="col-md-7">
+            <h2 class="heading-39291 mb-0">What We Do</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/001-stairs.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Creative Stairs</a></h3>
+              <p>Lorem ipsum dolor sit ame adipisicing elit. Perspiciatis incidunt distinctio voluptate .</p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/002-kitchen.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Kitchen Design</a></h3>
+              <p>Lorem ipsum dolor sit ame adipisicing elit. Perspiciatis incidunt distinctio voluptate .</p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/003-lamp.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Lamp Decoration</a></h3>
+              <p>Lorem ipsum dolor sit ame adipisicing elit. Perspiciatis incidunt distinctio voluptate .</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/004-blueprint.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Interior Blueprint</a></h3>
+              <p>Lorem ipsum dolor sit ame adipisicing elit. Perspiciatis incidunt distinctio voluptate .</p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/005-dinning-table.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Dinning Table</a></h3>
+              <p>Lorem ipsum dolor sit ame adipisicing elit. Perspiciatis incidunt distinctio voluptate .</p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-4 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-29193 text-center">
+              <span class="img-wrap mb-5">
+                <img src="fonts/flaticon/svg/006-pantone.svg" alt="Image" class="img-fluid">
+              </span>
+              <h3 class="mb-4"><a href="#">Modern Design</a></h3>
+              <p>Lorem ipsum dolor sit ame adipisicing elit. Perspiciatis incidunt distinctio voluptate .</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 align-items-center">
+          <div class="col-md-7">
+            <h2 class="heading-39291 mb-0">Our Projects</h2>
+          </div>
+          <div class="col-md-5 text-right">
+            <p class="mb-0"><a href="#" class="more-39291">View All Projects</a></p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link small"><img src="images/img_1.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Downside Up House</a></h3>
+              <span>Melbourne, Australia</span>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link"><img src="images/img_2.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Downside Up House</a></h3>
+              <span>Melbourne, Australia</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link"><img src="images/img_3.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Downside Up House</a></h3>
+              <span>Melbourne, Australia</span>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="media-02819">
+              <a href="#" class="img-link small"><img src="images/img_4.jpg" alt="Image" class="img-fluid"></a>
+              <h3><a href="#">Downside Up House</a></h3>
+              <span>Melbourne, Australia</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="site-section section-4">
+      <div class="container">
+
+        <div class="row justify-content-center text-center">
+          <div class="col-md-7">
+            <div class="slide-one-item owl-carousel">
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus totam sit delectus earum facere ex ea sunt, eos?</p>
+                <cite><span class="text-black">Mike Dorney</span> &mdash; <span class="text-muted">CEO and Co-Founder</span></cite>
+              </blockquote>
+
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p>Eligendi earum ad perferendis dolores, dolor quas. Ullam in, eaque mollitia suscipit id aspernatur rerum! Sit quibusdam ullam tempora quis, in voluptatum maiores veritatis recusandae!</p>
+                <cite><span class="text-black">James Smith</span> &mdash; <span class="text-muted">CEO and Co-Founder</span></cite>
+              </blockquote>
+
+              <blockquote class="testimonial-1">
+                <span class="quote quote-icon-wrap"><span class="icon-format_quote"></span></span>
+                <p> Officia, eius omnis rem non quis eos excepturi cumque sequi pariatur eaque quasi nihil dicta tempore voluptate culpa, veritatis incidunt voluptatibus qui?</p>
+                <cite><span class="text-black">Mike Dorney</span> &mdash; <span class="text-muted">CEO and Co-Founder</span></cite>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row  mb-5">
+          <div class="col-md-7">
+            <h2 class="heading-39291">Blog and Updates</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quos veniam magni totam, architecto earum dolor id obcaecati!</p>
+            
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+          <div class="col-lg-3 col-md-6 mb-5">
+            <div class="post-entry-1 h-100">
+              
+              <div class="post-entry-1-contents">
+                <span class="meta">Septermber 30, 2019</span>
+                <h2><a href="#">Iusto quos veniam magni totam</a></h2>
+                <p class="my-3"><a href="#" class="more-39291">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <div class="post-entry-1 h-100">
+              
+              <div class="post-entry-1-contents">
+                <span class="meta">Septermber 30, 2019</span>
+                <h2><a href="#">Iusto quos veniam magni totam</a></h2>
+                <p class="my-3"><a href="#" class="more-39291">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <div class="post-entry-1 h-100">
+              
+              <div class="post-entry-1-contents">
+                <span class="meta">Septermber 30, 2019</span>
+                <h2><a href="#">Iusto quos veniam magni totam</a></h2>
+                <p class="my-3"><a href="#" class="more-39291">Read More</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <div class="post-entry-1 h-100">
+              
+              <div class="post-entry-1-contents">
+                <span class="meta">Septermber 30, 2019</span>
+                <h2><a href="#">Iusto quos veniam magni totam</a></h2>
+                <p class="my-3"><a href="#" class="more-39291">Read More</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\home;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [home::class, 'index'])->name('index');
