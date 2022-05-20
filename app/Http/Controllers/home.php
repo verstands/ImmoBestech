76b6830a65382cb9bd8a817cbaff5fccf5340322
Rabby <rabbymbamu@gmail.com>
2022-05-20 16:08:12 +0200

    home final final

--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class home extends Controller
 {
-    public function home(){
-        return view('pages/home');
+    public function index(){
+        return view('pages.home');
     }
 }
